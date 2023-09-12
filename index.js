7727e888edc4bcce92c47deb45c136a9c0218f0a
Alberto Sierra <Sierra.Alberto@iCloud.com>
2023-09-11 20:21:55 -0700

    control flow lab complete

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(customer) {
   // Write your code here!
+  if (customer <= 400) {
+    return "This one is on me!";
+  } else if (customer <= 2000 && customer >= 400) {
+    return "That will be twenty bucks.";
+  } else if (2000 < customer && customer <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  // if (city === "NYC") {
+  //   return "Ok, sounds good.";
+  // } else {
+  //   return "No go.";
+  // }
+
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipString) {
+  // Write your code here!}
+  switch (tipString) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
 }
